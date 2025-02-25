b333768a3a0c2da021be964583b98650bd0116a5
juanda <juandacorreo@gmail.com>
2025-02-25 09:11:54 +0100

    login implementado

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,20 +1,36 @@
 import './Login.css';
+import { useRef } from 'react';
 
-function Login() {
+
+function Login({onLogin}) {
+
+  const userRef = useRef();
+  const pinRef = useRef();
+
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const user = userRef.current.value;
+    const pin = pinRef.current.value;
+    onLogin(user, pin);
+  };
+
   return (
     <form className="login">
       <input
         type="text"
         placeholder="user"
         className="login__input login__input--user"
+        ref={userRef}
       />
       <input
         type="text"
         placeholder="PIN"
         maxLength="4"
         className="login__input login__input--pin"
+        ref={pinRef}
       />
-      <button className="login__btn">&rarr;</button>
+      <button className="login__btn" onClick={handleLogin}>&rarr;</button>
     </form>
   );
 }
